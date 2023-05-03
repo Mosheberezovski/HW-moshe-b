de7ad8f2e52dac3409b3a5edf1128b990f0e3e99
MOSHE BEREZOVSKI <maberez28@gmail.com>
2023-05-03 11:08:32 +0300

    fnction

--- 05.01/js/main.js
+++ 05.01/js/main.js
@@ -1,3 +1,13 @@
-var stud= prompt("Please enter names of 5 students");
-stud= stud.split(" ").sort ().join(" ,");
-alert (stud)
+ss ()
+function ss() {
+  var stud = prompt("הזן בבקשה 5 שמות של תלמידים");
+  stud = stud.split(" ");
+  var sortedStud = stud.sort().join(" ,");
+  if (sortedStud === "") {
+     alert("בוק, למה לא הזנת כאן נתונים?");
+  } else if (stud.length <= 4) {
+    alert("ביקשתי 5 שמות ואתה הזנת רק " + stud.length + " שמות");
+  } else {
+    alert("השמות המסודרים בסדר אלפביתי הם: "+sortedStud);
+  }
+}
